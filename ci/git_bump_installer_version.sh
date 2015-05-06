350bbe49ec9f0e24fe29554911ddc2dc816bb5fe
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-06 16:36:34 -0700

    set git config before committing

--- ci/git_bump_installer_version.sh
+++ ci/git_bump_installer_version.sh
@@ -5,6 +5,9 @@ set -e -x
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 
+git config --global user.email "robot@concourse.ci"
+git config --global user.name "Concourse"
+
 git add cf-cli-release
 
 new_version_tag=$(cat cf-cli-release/tag)
