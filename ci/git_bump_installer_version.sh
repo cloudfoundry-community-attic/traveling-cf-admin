d5b889e68c85e64a061bb9f0f09836c8dfb284fb
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-06 17:04:49 -0700

    do not git-add/git-commit if no changes

--- ci/git_bump_installer_version.sh
+++ ci/git_bump_installer_version.sh
@@ -8,7 +8,10 @@ cd $DIR/..
 git config --global user.email "robot@concourse.ci"
 git config --global user.name "Concourse"
 
-git add cf-cli-release
-
-new_version_tag=$(cat cf-cli-release/tag)
-git commit -m "Bumping installer to ${new_version_tag} of traveling-cf-admin"
+if [[ "$(git status -s)X" != "X" ]]; then
+  new_version_tag=$(cat cf-cli-release/tag)
+  git add cf-cli-release
+  git commit -m "Bumping installer to ${new_version_tag} of traveling-cf-admin"
+else
+  echo "Version has not changed. Nothing to commit."
+fi
