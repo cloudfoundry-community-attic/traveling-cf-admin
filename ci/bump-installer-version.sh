9d36418badcede3cdf9fc0a6b91fc02b1af79a2c
Dr Nic Williams <drnicwilliams@gmail.com>
2016-02-12 17:33:25 +1000

    use 'git clone' to copy git repo

--- ci/bump-installer-version.sh
+++ ci/bump-installer-version.sh
@@ -3,11 +3,12 @@
 set -e -x
 
 new_version_tag=$(cat cf-cli-release/tag)
-cp -r traveling-cf-admin version-bumped-repo
-cd version-bumped-repo
 
 git config --global user.email "robot@concourse.ci"
 git config --global user.name "Concourse"
+
+git clone traveling-cf-admin version-bumped-repo
+cd version-bumped-repo
 
 if [[ "$(git status -s)X" != "X" ]]; then
   git add cf-cli-release
