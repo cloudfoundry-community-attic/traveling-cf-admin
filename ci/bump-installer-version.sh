05560abee88a5af29d3aed52baad2f59e1befab9
Dr Nic Williams <drnicwilliams@gmail.com>
2016-02-12 17:16:19 +1000

    outputs for task

--- ci/bump-installer-version.sh
+++ ci/bump-installer-version.sh
@@ -2,14 +2,14 @@
 
 set -e -x
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR/..
+new_version_tag=$(cat cf-cli-release/tag)
+cp -r traveling-cf-admin version-bumped-repo
+cd version-bumped-repo
 
 git config --global user.email "robot@concourse.ci"
 git config --global user.name "Concourse"
 
 if [[ "$(git status -s)X" != "X" ]]; then
-  new_version_tag=$(cat cf-cli-release/tag)
   git add cf-cli-release
   git commit -m "Bumping installer to ${new_version_tag} of traveling-cf-admin"
 else
